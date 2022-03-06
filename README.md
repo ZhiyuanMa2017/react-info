358936ae4c633543cbfa6330dad2e94b161bdc24
Zhiyuan Ma <631253076@qq.com>
2022-03-06 05:11:20 -0500

    airbnb page

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # Getting Started with Create React App
-
-![](screenshot.png)
+![](airbnb.png)
--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React from "react";
-import Main from "./conponents/Main";
-import Navbar from "./conponents/Navbar";
-
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Card from "./components/Card";
+import data from "./data";
 
 export default function App() {
+
+    const cards = data.map((item) => {
+        return (
+            <Card
+                key={item.id}
+                {...item}
+            />
+        );
+    });
+
     return (
         <>
             <Navbar/>
-            <Main/>
+            <Hero/>
+            <section className="cards-list">
+                {cards}
+            </section>
         </>
-    );
+    )
+        ;
 }
